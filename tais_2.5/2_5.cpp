b2a4e8f34610e5c5c2724cdc8c12b0b6b5e3cbe5
Washing-Mr8 <washing.22ml@gmail.com>
2025-09-23 22:18:45 +0100

    2_5 time limit

--- tais_2.5/2_5.cpp
+++ tais_2.5/2_5.cpp
@@ -14,11 +14,20 @@ using namespace std;
 
 /*@ <answer>
 
+Para resolver el problema necesitamos varias colas de prioridad de minimos ordenadas en funcion del orden en el que estan colocados los comics. Cada
+comic tiene un id que tomaremos en cuenta para saber cual elegir en caso de que tengan el mismo orden. Aprovechamos la lectura de datos de entrada para 
+averiguar de una vez cual es el mejor comic , que es el que tenga menor id (esto puede suponer un coste de O(M log N) debido a que M colas y ademas
+tenemos que insertar N comics en cada cola con su respectiva prioridad).
 
-COSTE: 
+Una vez tenemos todas las colas ordenadas, miraremos el elemento mas priotario de las M colas y nos quedaremos con el menor, eliminandolo y aumentando en 1
+el turno que nos toca esperar. Seguiremos haciendo esto hasta que encontremos el mejor ejemplar dando por finalizado el problema
 
+COSTE: los datos de entrada como dijimos anteriormente escalan a O(M log N), y como tenemos que obtener el top de cada cola para saber cual tenemos que elegir,
+recorremos M veces la cola de tamaño N, que como no siempre la recorremos al completo el coste es logN.
 
-COSTE EN ESPACIO ADICIONAL: O(N) siendo N el numero de pacientes a atender
+En resumen el coste de resolver el problema omitiendo la entrada es de -> O(M log N)
+
+COSTE EN ESPACIO ADICIONAL: O(M log N) siendo M el numero de colas y N la cantidad de comics en cada cola
 
  @ </answer> */
 
@@ -50,13 +59,13 @@ bool resuelveCaso() {
 	vector<priority_queue<Comic>> pilas(N);
 
 	int k = 0, id = 0, mejor_ejemplar = 100000000;
-	
+
 	for (int i = 0; i < N; i++) {
-		 cin >> k;
+		cin >> k;
 
 		while (k > 0) {
 			cin >> id;
-			pilas[i].push({id, k});
+			pilas[i].push({ id, k });
 			if (id < mejor_ejemplar) {
 				mejor_ejemplar = id;
 			}
@@ -64,39 +73,48 @@ bool resuelveCaso() {
 		}
 
 	}
-	
+
 	// escribir la solución
 
 	bool encontrado = false;
 	int turno = 1;
-
-	cout << "MEJOR EJEMPLAR: " << mejor_ejemplar << "\n";
-	for (int i = 0; i < N; i++) {
-		cout << "Pila de comics Numero " << i+1 << ": ";
-		while (!pilas[i].empty()) {
+	
+	while (!encontrado) {
+		int menor_index = 0, ultimo_comprado = 100000000;
+		for (int i = 0; i < N; i++) {
 			auto comic = pilas[i].top();
-			cout << comic.id << ", ";
-			pilas[i].pop();
+			if (comic.id < ultimo_comprado) {
+				ultimo_comprado = comic.id;
+				menor_index = i;
+			}
 		}
-		cout << "\n";
-	}
-
-
-	/*while (!encontrado || !comics.empty()) {
-		auto comic = comics.top();
-		cout << "ID " << comic.id << " y ORDEN" <<  comic.orden << ", ";
-		comics.pop();
+		auto comic = pilas[menor_index].top();
+		//cout << "ID: " << comic.id << " orden: " << comic.orden << ", ";
+		pilas[menor_index].pop();
 		if (comic.id == mejor_ejemplar) {
 			encontrado = true;
-			cout << "\n";
+			//cout << "\n";
 		}
 		else {
 			turno++;
 		}
-	}*/
-
+	}
+	
+	//DEBUG
+	/*
+		cout << "MEJOR EJEMPLAR: " << mejor_ejemplar << "\n";
+		for (int i = 0; i < N; i++) {
+			cout << "Pila de comics Numero " << i+1 << ": ";
+			while (!pilas[i].empty()) {
+				auto comic = pilas[i].top();
+				cout << comic.id << ", ";
+				pilas[i].pop();
+			}
+		cout << "\n";
+		}
+	
+	*/
 	cout << turno << "\n";
-
 
 	return true;
 }
