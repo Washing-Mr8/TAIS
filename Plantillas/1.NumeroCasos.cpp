4b7eecd775d92904d7548c6870fa5afd3a7fe797
Washing-Mr8 <washing.22ml@gmail.com>
2025-12-10 09:24:00 +0100

    Plantillas

---
+++ Plantillas/1.NumeroCasos.cpp
@@ -0,0 +1,60 @@
+/*@ <authors>
+ *
+ * E86 Nombre Apellidos
+ *
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+#include <...>
+using namespace std;
+
+#include "..."  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+// ================================================================
+// Escribe el código completo de tu solución aquí debajo
+// ================================================================
+//@ <answer>
+
+
+void resuelveCaso() {
+
+   // leer los datos de la entrada
+
+   // resolver el caso posiblemente llamando a otras funciones
+
+   // escribir la solución
+}
+
+//@ </answer>
+//  Lo que se escriba debajo de esta línea ya no forma parte de la solución.
+
+int main() {
+   // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+   ifstream in("casos.txt");
+   if (!in.is_open())
+      cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+   auto cinbuf = cin.rdbuf(in.rdbuf());
+#endif
+
+   int numCasos;
+   cin >> numCasos;
+   for (int i = 0; i < numCasos; ++i)
+      resuelveCaso();
+
+   // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+   cin.rdbuf(cinbuf);
+#endif
+   return 0;
+}
